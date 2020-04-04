215b80d6359a1bf1b3393f5c1973d118a1df9950
Nadeem Shaikh <nadeem.bs@gmail.com>
2020-04-03 20:32:14 -0700

    completed module2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -21,9 +21,21 @@ export default {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
-        { title: "American Gods", author: "Neil Gaiman" },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman" }
+        {
+          title: "Self-Reliance",
+          author: "Ralph Waldo Emerson",
+          finishedReading: true
+        },
+        {
+          title: "American Gods",
+          author: "Neil Gaiman",
+          finishedReading: false
+        },
+        {
+          title: "Amusing Ourselves to Death",
+          author: "Neil Postman",
+          finishedReading: true
+        }
       ]
     };
   },
@@ -35,7 +47,8 @@ export default {
     appendBook(bookData) {
       this.books.push({
         title: bookData.bookTitle,
-        author: bookData.bookAuthor
+        author: bookData.bookAuthor,
+        finishedReading: bookData.finishedReading
       });
     }
   }
