3ca91b398bd36d2c7ebe7bbc6b7c1a9456ee0e8c
Nadeem Shaikh <nadeem.bs@gmail.com>
2020-04-03 20:52:10 -0700

    completed module 3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,6 +4,14 @@
     <ul>
       <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
     </ul>
+    <hr />
+    <h2>Filtered Books by Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters" :key="filter">{{filter}}</option>
+    </select>
+    <ul>
+      <book-item v-for="book in filteredBooks" :key="book.id" :book="book"></book-item>
+    </ul>
     <br />
     <hr />
     <book-form @addBook="appendBook"></book-form>
@@ -11,6 +19,7 @@
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -20,24 +29,34 @@ export default {
     return {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
+      filters: ["bought", "borrowed"],
+      holding: "bought",
       books: [
         {
           title: "Self-Reliance",
           author: "Ralph Waldo Emerson",
-          finishedReading: true
+          finishedReading: true,
+          ownership: "borrowed"
         },
         {
           title: "American Gods",
           author: "Neil Gaiman",
-          finishedReading: false
+          finishedReading: false,
+          ownership: "bought"
         },
         {
           title: "Amusing Ourselves to Death",
           author: "Neil Postman",
-          finishedReading: true
+          finishedReading: true,
+          ownership: "borrowed"
         }
       ]
     };
+  },
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding]);
+    }
   },
   components: {
     BookItem,
@@ -48,7 +67,8 @@ export default {
       this.books.push({
         title: bookData.bookTitle,
         author: bookData.bookAuthor,
-        finishedReading: bookData.finishedReading
+        finishedReading: bookData.finishedReading,
+        ownership: bookData.ownership
       });
     }
   }
